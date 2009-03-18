0b0a01b34b10003ecd2c5b1341db6d25a78ca1a0
Olle Johansson <Olle@Johansson.com>
2009-03-18 14:47:27 +0000

    Re: #33 - Added simple blog page, just static html for now.

---
+++ pages/blog.php
@@ -0,0 +1,12 @@
+			<div class="centerDiv">
+			<div id="textDiv">
+			<h1>2009-03-18 - Update: Filename added to URL</h1>
+			<p>A new feature has been implemented. The filename of an uploaded file
+			can be appended to the URL, which will then be used as the filename of
+			the file when it is downloaded.</p>
+			<p>Also fixed so that images are displayed inline in the browser
+			instead of being downloaded. Some time in the future we might add
+			a viewing page as well.</p>
+
+			</div>
+			</div>
