da8628c11c65008dd21b54ad13c27835c8e884d1
Olle Johansson <Olle@Johansson.com>
2009-04-16 19:30:20 +0000

    Wrote blog entry about new share links.

--- pages/blog.php
+++ pages/blog.php
@@ -1,5 +1,14 @@
 			<div class="centerDiv">
 			<div id="textDiv">
+			<h1>2009-04-16 - New feature: Share link</h1>
+			<p>On the upload page we've added links to some popular online services
+			to share your link. At the moment it is possible to share the link on
+			Facebook, post to Twitter and create a TinyUrl.</p>
+			<p>More social bookmarking sites and services might be added in the 
+			future.</p>
+			</div>
+
+			<div id="textDiv">
 			<h1>2009-03-18 - Update: Filename added to URL</h1>
 			<p>A new feature has been implemented. The filename of an uploaded file
 			can be appended to the URL, which will then be used as the filename of
