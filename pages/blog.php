7b3ec92054493d49c727405cec760407f6857223
Olle Johansson <Olle@Johansson.com>
2010-02-10 08:56:49 +0000

    Added a blog note.

--- pages/blog.php
+++ pages/blog.php
@@ -1,20 +1,27 @@
 			<div class="centerDiv">
 			<div id="textDiv">
-			<h1>2009-04-19 - New features: Public files and emailing</h1>
+			<h2>2010-02-10 - Bug fixes</h2>
+			<p>A few bugs have been squashed recently. Delete works properly
+			again, and delete link has been removed from public listing.
+			Also added detailed info on the extra options, the full description
+			is shown as a tooltip in file lists and did some minor html changes.
+			Backend code has been updated as well, to increase stability.</p>
+
+			<h2>2009-04-19 - New features: Public files and emailing</h2>
 			<p>Files can now be set to public so that they show up in the
 		        public file listing. It's now also possible to enter an email
                         address to send information about the file to upon upload.
 			Added a proper 404 page when a file wasn't found.
 			When deleting files, a full page with information is shown.</p>
 
-			<h1>2009-04-16 - New feature: Share link</h1>
+			<h2>2009-04-16 - New feature: Share link</h2>
 			<p>On the upload page we've added links to some popular online services
 			to share your link. At the moment it is possible to share the link on
 			Facebook, post to Twitter and create a TinyUrl.</p>
 			<p>More social bookmarking sites and services might be added in the 
 			future.</p>
 
-			<h1>2009-03-18 - Update: Filename added to URL</h1>
+			<h2>2009-03-18 - Update: Filename added to URL</h2>
 			<p>A new feature has been implemented. The filename of an uploaded file
 			can be appended to the URL, which will then be used as the filename of
 			the file when it is downloaded.</p>
