8a82ed37ecf6020f3d780e122711b381180cab0a
Olle Johansson <Olle@Johansson.com>
2010-07-29 00:23:33 +0000

    MINOR: License added.

--- classes/VCScanner/VirusTotalHash.php
+++ classes/VCScanner/VirusTotalHash.php
@@ -1,9 +1,22 @@
 <?php
+/*
+Copyright 2010 Olle Johansson. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+*/
+
 /**
  * VirusChecker scanner plugin to check MD5/SHA1 hash against VirusTotal.com
  * @author Olle@Johansson.com
  */
-
 class VCScanner_VirusTotalHash implements VCScanner
 {
     const URL = 'http://www.virustotal.com/vt/en/consultamd5';
