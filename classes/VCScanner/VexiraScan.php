babaac01c86a679929ada9664950b29a42af885e
Olle Johansson <Olle@Johansson.com>
2010-11-18 14:43:13 +0000

    MINOR: Added MIT license file and license text in all scripts.

--- classes/VCScanner/VexiraScan.php
+++ classes/VCScanner/VexiraScan.php
@@ -1,16 +1,33 @@
 <?php
+/**
+ * UploadBin - Site for sending files.
+ *
+ * @package uploadbin
+ * @author Olle@Johansson.com and Mattias Johansson
+ */
+
 /*
-Copyright 2010 Olle Johansson. All rights reserved.
+The MIT License
 
-Redistribution and use in source and binary forms, with or without modification, are
-permitted provided that the following conditions are met:
+Copyright (c) 2010 Olle Johansson
 
-   1. Redistributions of source code must retain the above copyright notice, this list of
-      conditions and the following disclaimer.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-   2. Redistributions in binary form must reproduce the above copyright notice, this list
-      of conditions and the following disclaimer in the documentation and/or other materials
-      provided with the distribution.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 
 /**
@@ -20,7 +37,7 @@ permitted provided that the following conditions are met:
 class VCScanner_VexiraScan implements VCScanner
 {
     /**
-     * Runs clamscan on the given file.
+     * Runs Vexira scan on the given file.
      */
     public function scan($filename) {
         $output = array();
